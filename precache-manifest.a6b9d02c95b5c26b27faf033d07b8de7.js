ddc4e0390395c13bd8b67c1164f15c45b7f6b116
KyPV <bp.clashofclan@gmail.com>
2019-07-02 11:30:13 +0700

    Updates

---
+++ precache-manifest.a6b9d02c95b5c26b27faf033d07b8de7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40a5ac75e7311b392994c50a22b46a7e",
+    "url": "/conduitReactjs/index.html"
+  },
+  {
+    "revision": "c0b42406b10f58262ba8",
+    "url": "/conduitReactjs/static/css/main.ed97d07f.chunk.css"
+  },
+  {
+    "revision": "44d77282f51157e5cbeb",
+    "url": "/conduitReactjs/static/js/2.f01dc2a4.chunk.js"
+  },
+  {
+    "revision": "c0b42406b10f58262ba8",
+    "url": "/conduitReactjs/static/js/main.4e6b17f4.chunk.js"
+  },
+  {
+    "revision": "1a06e339af5ae562cdbe",
+    "url": "/conduitReactjs/static/js/runtime~main.0e57fe68.js"
+  }
+]);
